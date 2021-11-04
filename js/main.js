d9b7872398f65f5690e71de3557629d2446d7714
Ryan de Menezes <menezesryan1010@gmail.com>
2021-11-03 22:34:31 -0300

    Finalizando estrutura do site

--- js/main.js
+++ js/main.js
@@ -1,12 +1,37 @@
 $(document).ready(function(){
     $('.social').hide()
 
+    // OWRCAROUSEL
     $('.owl-carousel').owlCarousel({
         loop: true,
         margin: 10,
         nav: true,
         dots: false,
-        items: 4
+        items: 3
+    })
+
+    // MASK
+    $('*').delegate('.date-mask', 'focus', function(){
+        $(this).mask('99/99/9999')
+    })
+
+    $('*').delegate('.cpf-mask', 'focus', function(){
+        $(this).mask('999.999.999-99')
+    })
+
+    $('*').delegate('.phone-mask', 'focus', function(){
+        $(this).mask('(99)99999-9999')
+    })
+
+    // FORM-VALIDATE
+    $('*').delegate('.form-validate', 'focus', function(){
+        $(this).validate({
+            errorElement: 'span',
+            messages: {
+                required: 'Este campo é obrigatório',
+                email: 'Por favor entre com um email válido'
+            }
+        })
     })
 
     $(document).scroll(function(){
